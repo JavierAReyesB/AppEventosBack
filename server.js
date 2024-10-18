d0c6232fa5b68d9f133afa9f67f2738bef5ac104
JavierAReyesB <reyes.bahamondej@gmail.com>
2024-10-18 22:31:42 +0200

    Actualizado

--- server.js
+++ server.js
@@ -40,10 +40,12 @@ app.options('*', (req, res) => {
     'Access-Control-Allow-Origin',
     'https://app-eventos-front.vercel.app'
   )
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.sendStatus(204) // No Content
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*') // Permite todas las solicitudes
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+  })
 })
 
 // Middleware para parsear JSON
